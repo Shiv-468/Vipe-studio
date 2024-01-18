29dc9380dd35628e61dd4f25e194b3a0623fc9b0
Shiv-468 <shivmuwal.147@gmail.com>
2024-01-18 11:22:33 +0530

    _font brown

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,8 @@ module.exports = {
         'Gilroy-Bold': ('Gilroy-Bold'),
         'Jost': ('"Jost", sans-serif'),
         'Inter': ('Inter'),
+        'Fontleroy-Brown': ('Fontleroy-Brown'),
+        'fontbrown-2':('fontbrown-2'),
       },
       boxShadow: {
         'gray': ('0px 20px 60.571px 0px #E5E9F6'),
